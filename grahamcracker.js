c6a9735692b5840f4683f27bc36e3c4b23970d80
Dylan O'Carroll <dylanocarroll@Dylans-Laptop.local>
2013-06-15 16:43:16 -0700

    twerkin on responsive templates

--- grahamcracker.js
+++ grahamcracker.js
@@ -1,15 +1,22 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to grahamcracker.";
+
+
+  Template.App.username = function(){
+    return Session.get('username');
   };
 
-  Template.hello.events({
-    'click input' : function () {
-      window.open("https://api.instagram.com/oauth/authorize/?client_id=9bc60db22405424197bb2a287063e5f6&redirect_uri=http://gramcracker.meteor.com/_oauth/instagram&response_type=code", "instagram");
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+  Template.App.judge = function(){
+    return Session.get('username') === "judge";
+  };
+
+  Template.login.events({
+    'click button': function(){
+      var username = document.getElementById("submitUsername").value;
+      Session.set("username", username);
+      Session.set("player", true);
     }
   });
+
 }
 
 if (Meteor.isServer) {
