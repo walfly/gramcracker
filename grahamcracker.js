1451f9a4fb3eb2f7fd39e7e9d67c6e14df2e85a2
Walker Flynn <walker.d.flynn@gmail.com>
2013-06-15 18:31:07 -0700

    pre test logic

--- grahamcracker.js
+++ grahamcracker.js
@@ -1,3 +1,5 @@
+Players = new Meteor.Collection('players');
+Rounds = new Meteor.Collection('rounds');
 if (Meteor.isClient) {
 
 
@@ -5,22 +7,61 @@ if (Meteor.isClient) {
     return Session.get('username');
   };
 
+  Template.App.rounds = function(){
+    return Rounds.find().count() > 0;
+  }
+
+  Template.App.enoughPlayers = function(){
+    return Players.find().count() > 3;
+  }
+
   Template.App.judge = function(){
-    return Session.get('username') === "judge";
+    if(Players.find({username: Session.get('username')}).isJudge){
+      return true;
+    } else {
+      return false;
+    }
   };
 
   Template.login.events({
     'click button': function(){
       var username = document.getElementById("submitUsername").value;
       Session.set("username", username);
-      Session.set("player", true);
+      Players.insert({
+        id: Players.find().count(),
+        username: username, 
+        score: 0, 
+        accessToken: '44947478.9bc60db.7d906225574340669fc0f9b21a6e9523', 
+        isJudge: false});
     }
   });
+  Template.start.events({
+    'click button': function(){
+      Rounds.insert({
+        submissions: {},
+        winner: {}
+      });
+      Meteor.call('setFirstJudge', function(){
+        console.log(Players.find({isJudge: false}).count());
+        console.log(Rounds.find());
+      });
+    }
+  })
 
 }
 
+Meteor.methods({
+  setFirstJudge: function () {
+    console.log('sup');
+    //Players.update({}, {isJudge: false}, {multi: true});
+    Players.update({id: 0}, {$set: {isJudge: true}});
+  }
+})
+
 if (Meteor.isServer) {
   Meteor.startup(function () {
+    Players.remove({});
+    Rounds.remove({});
     // code to run on server at startup
   });
 }
